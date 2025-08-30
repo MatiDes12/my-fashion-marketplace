cbd4efbd5801b3a27a5a13313ee47ff8beaa588b
Mati1514 <matiman1415@gmail.com>
2025-08-29 20:40:29 -0400

    Update Stripe logo to use CDN URL instead of local SVG

--- src/components/SubscriptionPaymentSelector.tsx
+++ src/components/SubscriptionPaymentSelector.tsx
@@ -28,7 +28,7 @@ const PAYMENT_METHODS: PaymentMethod[] = [
   {
     id: 'stripe',
     name: 'Stripe',
-    logo: '/images/payment-methods/stripe.svg',
+    logo: 'https://cdn.brandfetch.io/idxAg10C0L/theme/dark/logo.svg?c=1bxid64Mup7aczewSAYMX&t=1746435914582',
     description: 'Pay securely with credit/debit cards',
     isAvailable: true
   }
